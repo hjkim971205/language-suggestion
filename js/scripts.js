0445e536ce78fb1958a1421b99324b134b977830
Hyung Jin Kim <hyungjinkim1205@gmail.com>
2023-01-13 16:06:45 -0800

    add front and back options

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,18 @@ function suggestLanguage(event) {
     event.preventDefault();
     const nameInput = document.getElementById("nameInput").value;
     document.querySelector("span#survey").innerText = nameInput;
+
+    let potential;
+    let frontEnd;
+    let backEnd;
+    const frontBack = document.querySelector("input[name='frontBack']:checked").value;
+    if (frontBack === "front") {
+        potential = "frontEnd";
+    } else{ 
+        potential = "backEnd"; //remove quotes
+    }
+
+    document.getElementById("output").innerText = potential;
 }
 
 window.addEventListener("load", function () {
